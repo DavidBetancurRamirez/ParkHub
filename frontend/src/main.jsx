9e45239b2b5d298bade82cb1769a9d0b04ab2005
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-09-22 20:27:57 -0500

    Ruteo y contextos implementados

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import './main.css';
+
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserContextProvider } from './context/userContext';
+import { MessageContextProvider } from './context/messageContext';
+import Sesion from './components/Sesion';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    {/* Contextos */}
+    <MessageContextProvider>
+      <UserContextProvider>
+
+          {/* Ruteo */}
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<App />} />
+              <Route path='/sesion' element={<Sesion />} />
+            </Routes>
+          </BrowserRouter>
+
+      </UserContextProvider>
+    </MessageContextProvider>
+
+  </React.StrictMode>
 )
